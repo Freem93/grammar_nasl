79748590b6471b0aee4b87d3e27e776455f9a368
Darina Becker <das5013@yandex.ru>
2017-05-14 17:46:11 +0300

    change start.bat

--- start.bat
+++ start.bat
@@ -6,5 +6,17 @@ flex.exe lex.l
 bison.exe -dy nasl_grammar.y
 gcc lex.yy.c y_tab.c -o parse.exe
 
+parse.exe < tests\signed.nasl
+parse.exe < tests\test_blowfish.nasl
+parse.exe < tests\test_bn.nasl
+parse.exe < tests\test_dh.nasl
+parse.exe < tests\test_dsa.nasl
+parse.exe < tests\test_hexstr.nasl
+parse.exe < tests\test_isotime.nasl
+parse.exe < tests\test_md.nasl
+parse.exe < tests\test_privkey.nasl
+parse.exe < tests\test_rsa.nasl
 parse.exe < tests\test_socket.nasl
+parse.exe < tests\testsuiteinit.nasl
+parse.exe < tests\testsuitesummary.nasl
 pause
